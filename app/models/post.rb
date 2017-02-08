72932ad7a54473c279951a00ef22c2ff3e0e0061
Moran <moran@simplee.com>
2017-02-08 10:23:56 +0200

    adding image to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
   belongs_to :topic
+  mount_uploader :image, ImageUploader
 
   default_scope { order('created_at DESC') }
 
