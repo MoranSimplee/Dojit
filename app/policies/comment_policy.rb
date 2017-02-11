c79c463b8c5cf582fb35b4f06c23e01ac898d7c7
Moran <moran@simplee.com>
2017-02-11 21:39:31 +0200

    Completed destroy checkpoint

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -3,5 +3,8 @@ class CommentPolicy < ApplicationPolicy
     def create?
       user.present? && user.admin?
     end
-
+    
+    def destroy?
+      user.present? && (record.user == user || user.admin? || user.moderator?)
+    end
 end
