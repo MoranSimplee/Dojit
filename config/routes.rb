ac299dff75af18306db71b5a2d08cd831bc3c70d
Moran <moran@simplee.com>
2017-02-12 17:27:26 +0200

    COmpleted voting assignment

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/up-vote' => 'votes#up_vote', as: :up_vote
+  post '/down-vote' => 'votes#down_vote', as: :down_vote
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -31,13 +31,19 @@ topics = Topic.all
 
 # Create Posts
 50.times do
-  Post.create!(
+  post = Post.create!(
     user:   users.sample,
-    topic: topic.sample,
+    topic: topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
+
+    # set the created_at to a time within the past year
+  post.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
+  post.update_rank
 end
+
+
 posts = Post.all
 
 # Create Comments
@@ -45,7 +51,7 @@ posts = Post.all
   Comment.create!(
     #user:users.sample,
     post: posts.sample,
-    body: Faker::Lorem.paragraph
+    body: Faker::Lorem.paragraph,
     user: users.sample
   )
 end
